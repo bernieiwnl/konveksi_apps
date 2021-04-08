bb84ebaaae2e76f746d8cea26ab65c82c08238b5
SMEAG <79180204+bernieiwnl@users.noreply.github.com>
2021-04-08 10:51:37 +0700

    Fitur Pesan
    Penambahan Fitur Pesan

---
+++ KonveksiClass/DaftarPesan.cs
@@ -0,0 +1,126 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonveksiClass
+{
+    public class DaftarPesan
+    {
+        private List<pesan> listpesan;
+        private string kodeTerbaru;
+
+        public string KodeTerbaru { get => kodeTerbaru; set => kodeTerbaru = value; }
+
+        public List<pesan> Listpesan { get => listpesan; set => listpesan = value; }
+
+        public int JumlahBahan
+        {
+            get { return listpesan.Count; }
+        }
+
+        public DaftarPesan()
+        {
+            Listpesan = new List<pesan>();
+            kodeTerbaru = "P1";
+        }
+
+        public string CariData(string kriteria, string nilaiKriteria)
+        {
+            Koneksi k = new Koneksi();
+            k.Connect();
+
+            string sql = "SELECT pesan.id, p1.id, p1.nama, p2.id, p2.nama, keterangan FROM pesan INNER JOIN pegawai as p1 on pesan.idTujuan = p1.id INNER JOIN pegawai as p2 on pesan.idPenerima = p2.id " +
+                         "WHERE " + kriteria + " LIKE '%" + nilaiKriteria + "%'";
+
+            MySqlCommand c = new MySqlCommand(sql, k.KoneksiDB);
+
+            try
+            {
+                MySqlDataReader data = c.ExecuteReader();
+
+                while (data.Read() == true)
+                {
+                    string id = data.GetValue(0).ToString();
+                    string idTujuan = data.GetValue(1).ToString();
+                    string namaTujuan = data.GetValue(2).ToString();
+                    string idPenerima = data.GetValue(3).ToString();
+                    string namaPenerima = data.GetValue(4).ToString();
+                    string keterangan = data.GetValue(5).ToString();
+
+                    pegawai p1 = new pegawai(idTujuan, namaTujuan, default, null, null, null, null, null);
+                    pegawai p2 = new pegawai(idPenerima, namaPenerima, default, null, null, null, null, null);
+
+                    pesan psn = new pesan(id, p1, p2, keterangan);
+
+                    listpesan.Add(psn);
+                }
+                c.Dispose();
+                data.Dispose();
+
+                return "sukses";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public string GenerateKode()
+        {
+            Koneksi k = new Koneksi();
+            k.Connect();
+
+            string sql = "SELECT SUBSTRING(id,2) FROM pesan ORDER BY id DESC LIMIT 1";
+
+            MySqlCommand c = new MySqlCommand(sql, k.KoneksiDB);
+
+            try
+            {
+                MySqlDataReader data = c.ExecuteReader();
+                if (data.Read() == true)
+                {
+                    int kdTerbaru = int.Parse(data.GetValue(0).ToString()) + 1;
+                    kodeTerbaru = kdTerbaru.ToString();
+
+                    if (kodeTerbaru.Length == 1)
+                    {
+                        kodeTerbaru = "P" + kodeTerbaru;
+                    }
+                }
+                c.Dispose();
+                data.Dispose();
+
+                return "sukses";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public string TambahData(pesan p)
+        {
+            Koneksi k = new Koneksi();
+            k.Connect();
+
+            string sql = "INSERT INTO satuan(idPesan, idTujuan, idPenerima, keterangan) VALUES ('" + p.IdPesan + "','" + p.IdTujuan + "','" + p.IdPenerima + "','" + p.Keterangan + "')";
+
+            MySqlCommand c = new MySqlCommand(sql, k.KoneksiDB);
+
+            try
+            {
+                c.ExecuteNonQuery();
+                return "sukses";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+    }
+}
---
+++ KonveksiClass/pesan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonveksiClass
+{
+    public class pesan
+    {
+        private string idPesan;
+        private pegawai idTujuan;
+        private pegawai idPenerima;
+        private string keterangan;
+
+        public string IdPesan { get => idPesan; set => idPesan = value; }
+        public pegawai IdTujuan { get => idTujuan; set => idTujuan = value; }
+        public pegawai IdPenerima { get => idPenerima; set => idPenerima = value; }
+        public string Keterangan { get => keterangan; set => keterangan = value; }
+
+        public pesan()
+        {
+            this.idPesan = "";
+            this.idTujuan = new pegawai();
+            this.idPenerima = new pegawai();
+            this.keterangan = "";
+        }
+
+        public pesan(string id, pegawai tujuan, pegawai penerima, string keterangan)
+        {
+            this.idPesan = id;
+            this.idTujuan = tujuan;
+            this.idPenerima = penerima;
+            this.keterangan = keterangan;
+        }
+
+
+    }
+}
